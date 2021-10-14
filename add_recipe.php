6dfdec0fc9205b5a681432c701560d1df0b482b7
Matyáš Cron <okay728pravy@gmail.com>
2021-10-14 21:05:42 +0200

    Processing moved to add_recipe

--- add_recipe.php
+++ add_recipe.php
@@ -6,12 +6,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <title>Document</title>
+
+    <?php 
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "demo";
+
+    $db_status = 2;
+
+    preg_match("/.+(\/.+)$/", $_SERVER["HTTP_REFERER"], $match);
+
+    if(isset($match[1]) && $match[1] == $_SERVER["REQUEST_URI"] && count($_POST) != 0){
+        
+        $conn = new mysqli($host, $user, $pass, $dbname);
+        if($conn->connect_error){
+            $db_status = -1;
+            die("Connection to DB failed: " . $conn->connect_error);
+        }
+
+        if($_POST['cas'] > 120){
+            $_POST['cas'] = 120;
+        } elseif($_POST['cas'] < 10){
+            $_POST['cas'] = 10;
+        }
+
+        $stmt = $conn->prepare("INSERT INTO recepty (jmeno, popis, postup, casPripravy, autor) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssis", $nazev, $popis, $postup, $casik, $jmeno);
+
+        $nazev = htmlspecialchars($_POST['nazev'], ENT_QUOTES);
+        $popis = htmlspecialchars($_POST['popis'], ENT_QUOTES);
+        $postup = htmlspecialchars($_POST['postup'], ENT_QUOTES);
+        $casik = htmlspecialchars($_POST['cas'], ENT_QUOTES);
+        $jmeno = htmlspecialchars($_POST['jmeno'], ENT_QUOTES);
+        
+        if($stmt->execute()){
+            $db_status = 1;
+            #header("Location: ". "http://kucharka.testing/");
+        }
+        else{
+            $db_status = 0;
+        }
+    }
+    ?>
+
 </head>
 <body>
     <?php include "web_parts/header.php"?>
     <main>
     <h1>Přidání receptu</h1>
-    <form action="process.php" method="POST">
+    <form action="add_recipe.php" method="POST">
         <fieldset>
             <legend>Hlavní informace</legend>
             <div>
@@ -41,6 +86,21 @@
         <div class="submit-btn">
             <input type="submit" value="Odeslat">
         </div>
+
+        <div class="notif-box">
+            <?php 
+                switch($db_status){
+                    case -1:
+                    case 0:
+                        echo "<div class='fail'><p>Nepodařilo se vložit do databáze :(</p></div>";
+                        break;
+                    case 1:
+                        echo "<div class='pass'><p class='pass'>Vložení úspěšné :)</p></div>";            
+                        break;
+                }
+            ?>
+        </div>
+
     </form>
     </main>
     <?php include "web_parts/footer.php"?>
--- process.php
+++ process.php
@@ -14,16 +14,18 @@
     }
 
 
-    $nazev = $_POST['nazev'];
-    $popis = $_POST['popis'];
-    $postup = $_POST['postup'];
-    $casik = $_POST['cas'];
-    $jmeno = $_POST['jmeno'];
+    $stmt = $conn->prepare("INSERT INTO recepty (jmeno, popis, postup, casPripravy, autor) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssis", $nazev, $popis, $postup, $casik, $jmeno);
 
-    $query = "INSERT INTO recepty (jmeno, popis, postup, casPripravy, autor) VALUES ('$nazev', '$popis', '$postup', '$casik', '$jmeno')";
-    if(mysqli_query($conn, $query)){
+    $nazev = htmlspecialchars($_POST['nazev'], ENT_QUOTES);
+    $popis = htmlspecialchars($_POST['popis'], ENT_QUOTES);
+    $postup = htmlspecialchars($_POST['postup'], ENT_QUOTES);
+    $casik = htmlspecialchars($_POST['cas'], ENT_QUOTES);
+    $jmeno = htmlspecialchars($_POST['jmeno'], ENT_QUOTES);
+    
+    if($stmt->execute()){
         echo "Vlozeni uspesne";
-        header("Location: ". "http://kucharka.testing/");
+        #header("Location: ". "http://kucharka.testing/");
     }
     else{
         echo "Nastala chyba: " . mysqli_error($conn);
